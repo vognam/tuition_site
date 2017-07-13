d8ebcd8485a818d25b3e84b6777e96db83c5658a
Vignesh <vignesh.uma@gmail.com>
2017-07-14 00:16:24 +0100

    set environment variables

--- tuition_site/settings.py
+++ tuition_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jhp2cq*zj2h&wahic*#j5@l$ss^cdk01hsn(nx$mmsa9ka0-%3'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -129,5 +129,5 @@ EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'vignesh.uma1@gmail.com'
-EMAIL_HOST_PASSWORD = 'anotheremail'
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 
